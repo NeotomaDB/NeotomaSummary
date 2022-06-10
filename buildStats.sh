6096f1c8062ca11abddbaeb678eeb4cc5a88f277
SimonGoring <goring@wisc.edu>
2022-06-10 09:32:15 -0700

    Push to add docker reprex to github.
    Cleaning up the code to make sure it actually compiles, removing some extraneous files. Moved outputs to the `outputs` folder, with run coming from the bash script.

--- buildStats.sh
+++ buildStats.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+export DBNAME=neotoma
+export HOST=localhost
+export PORT=5432
+export USER=postgres    
+export PASSWORD=postgres
 Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
 Rscript neotomaplots.R
