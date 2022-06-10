95d7fd0385f7059902f4bb618585a95de2e7428f
SimonGoring <goring@wisc.edu>
2022-06-10 09:32:15 -0700

    Push to add docker reprex to github.
    Cleaning up the code to make sure it actually compiles, removing some extraneous files. Moved outputs to the `outputs` folder, with run coming from the bash script.

--- neotomaplots.R
+++ neotomaplots.R
@@ -9,9 +9,9 @@ library(lubridate)
 library(scales)
 
 datasettypes <- function(x) {
-  call <- httr::GET('http://api.neotomadb.org/v2.0/data/summary/dstypemonth',
+  call <- httr::GET("http://api.neotomadb.org/v2.0/data/summary/dstypemonth",
                     query = list(start = x, end = x + 1))
-  result <- jsonlite::fromJSON(httr::content(call, as = 'text'))$data$data
+  result <- jsonlite::fromJSON(httr::content(call, as = "text"))$data$data
   result$month <- x
   result$counts <- as.numeric(result$counts)
   return(result)
@@ -34,22 +34,29 @@ rev_date <- c_trans("reverse", "time")
 monthly_change <- 0:18 %>% map(datasettypes) %>% bind_rows()
 
 mc2 <- fct_collapse(monthly_change$datasettype,
-                               biomarker = 'biomarker',
-                               charcoal = c('charcoal', 'macrocharcoal', 'microcharcoal', 'charcoal surface sample'),
-                               diatom = c('diatom', 'diatom surface sample'),
-                               geochemistry = 'geochemistry',
-                               insect = 'insect',
-                               LOI = 'loss-on-ignition',
-                               macroinvertebrate = 'macroinvertebrate',
-                               ostracode = c('ostracode surface sample'),
-                               sedimentology = c('physical sedimentology','paleomagnetic','X-ray fluorescence (XRF)'),
-                               'phytolith' = 'phytolith',
-                               'plant macrofossil' = 'plant macrofossil',
-                               pollen = c('pollen', 'pollen trap', 'pollen surface sample'),
-                               'stable isotope' = 'specimen stable isotope',
-                               'testate amoebae' = c('testate amoebae', 'testate amoebae surface sample'),
-                               'vertebrate fauna' = 'vertebrate fauna',
-                               'water chemistry' = 'water chemistry')
+                    biomarker = "biomarker",
+                    charcoal = c("charcoal",
+                                "macrocharcoal",
+                                "microcharcoal",
+                                "charcoal surface sample"),
+                    diatom = c("diatom", "diatom surface sample"),
+                    geochemistry = "geochemistry",
+                    insect = "insect",
+                    LOI = "loss-on-ignition",
+                    macroinvertebrate = "macroinvertebrate",
+                    ostracode = c("ostracode surface sample"),
+                    sedimentology = c("physical sedimentology",
+                                      "paleomagnetic",
+                                      "X-ray fluorescence (XRF)"),
+                    "phytolith" = "phytolith",
+                    "plant macrofossil" = "plant macrofossil",
+                    pollen = c("pollen", "pollen trap",
+                              "pollen surface sample"),
+                    "stable isotope" = "specimen stable isotope",
+                    "testate amoebae" = c("testate amoebae",
+                                          "testate amoebae surface sample"),
+                    "vertebrate fauna" = "vertebrate fauna",
+                    "water chemistry" = "water chemistry")
 
 monthly_change$datasettype <- mc2
 
@@ -59,24 +66,24 @@ monthly_out <- monthly_change %>%
   mutate(month = (Sys.Date() %m-% months(month, abbreviate = FALSE)))
 
 out <- ggplot(monthly_out, aes(x = month, y = count, fill = datasettype)) +
-  geom_bar(stat = 'identity') +
+  geom_bar(stat = "identity") +
   theme_tufte() +
   scale_fill_viridis_d(name = "Dataset Type") +
   scale_x_date(date_labels = "%b %Y") +
-  xlab('Month') +
-  ylab('Number of Datasets Uploaded') +
-  theme(axis.title = element_text(face = 'bold', size = 18),
-        axis.text = element_text(family = 'sans-serif', size = 10))
+  xlab("Month") +
+  ylab("Number of Datasets Uploaded") +
+  theme(axis.title = element_text(face = "bold", size = 18),
+        axis.text = element_text(family = "sans-serif", size = 10))
 
-ggsave('datasetsPerMonth.png', out, width = 8, height = 6, units = "in")
-ggsave('datasetsPerMonth.svg', out, width = 8, height = 6, units = "in")
+ggsave("datasetsPerMonth.png", out, width = 8, height = 6, units = "in")
+ggsave("datasetsPerMonth.svg", out, width = 8, height = 6, units = "in")
 
 ##################
 
 datasetdbs <- function(x) {
-  call <- httr::GET('http://api.neotomadb.org/v2.0/data/summary/dsdbmonth',
+  call <- httr::GET("http://api.neotomadb.org/v2.0/data/summary/dsdbmonth",
                     query = list(start = x, end = x + 1))
-  result <- jsonlite::fromJSON(httr::content(call, as = 'text'))$data$data
+  result <- jsonlite::fromJSON(httr::content(call, as = "text"))$data$data
   result$month <- x
   result$counts <- as.numeric(result$counts)
   return(result)
@@ -89,24 +96,24 @@ monthly_change <- 0:16 %>%
 
 out <- ggplot(monthly_change,
               aes(x = month, y = counts, fill = databasename)) +
-  geom_bar(stat = 'identity') +
+  geom_bar(stat = "identity") +
   theme_tufte() +
-  scale_fill_viridis_d(name = 'Constituent Database') +
+  scale_fill_viridis_d(name = "Constituent Database") +
   scale_x_date(date_labels = "%b %Y") +
-  xlab('Month') +
-  ylab('Number of Datasets Uploaded') +
-  theme(axis.title = element_text(face = 'bold', size = 18),
-        axis.text = element_text(family = 'sans-serif', size = 10))
+  xlab("Month") +
+  ylab("Number of Datasets Uploaded") +
+  theme(axis.title = element_text(face = "bold", size = 18),
+        axis.text = element_text(family = "sans-serif", size = 10))
 
-ggsave('constDBPerMonth.png', out, width = 16, height = 4, units = "in")
-ggsave('constDBPerMonth.svg', out, width = 16, height = 4, units = "in")
+ggsave("constDBPerMonth.png", out, width = 16, height = 4, units = "in")
+ggsave("constDBPerMonth.svg", out, width = 16, height = 4, units = "in")
 
 ######################
 
 rawmonthly <- function(x) {
-  call <- httr::GET('http://api.neotomadb.org/v2.0/data/summary/rawbymonth',
+  call <- httr::GET("http://api.neotomadb.org/v2.0/data/summary/rawbymonth",
                     query = list(start = x, end = 500))
-  result <- jsonlite::fromJSON(httr::content(call, as = 'text'))$data$data
+  result <- jsonlite::fromJSON(httr::content(call, as = "text"))$data$data
   result$month <- x
   return(result)
 }
@@ -124,10 +131,10 @@ out <- ggplot(output, aes(x = month, y = as.numeric(count), group = obs)) +
   theme_tufte() +
   scale_fill_viridis_d() +
   scale_x_date(date_labels = "%Y") +
-  xlab('Month') +
-  ylab('Number of Datasets Uploaded') +
-  theme(axis.title = element_text(face = 'bold', size = 18),
-        axis.text = element_text(family = 'sans-serif', size = 10))
+  xlab("Month") +
+  ylab("Number of Datasets Uploaded") +
+  theme(axis.title = element_text(face = "bold", size = 18),
+        axis.text = element_text(family = "sans-serif", size = 10))
 
-ggsave('ChangeOverTime.png', out, width = 8, height = 4, units = "in")
-ggsave('ChangeOverTime.svg', out, width = 8, height = 4, units = "in")
+ggsave("ChangeOverTime.png", out, width = 8, height = 4, units = "in")
+ggsave("ChangeOverTime.svg", out, width = 8, height = 4, units = "in")
