9bb4c327289d529b93deba63b912db57f93ef46b
Simon Goring <simon.j.goring@gmail.com>
2022-07-19 10:04:28 -0700

    Running the build

--- buildStats.sh
+++ buildStats.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export DBNAME=neotoma
-export HOST=localhost
+export HOST=db5.cei.psu.edu
 export PORT=5432
-export USER=postgres    
-export PASSWORD=postgres
-Rscript -e "rmarkdown::render('outputs/StateoftheDB.Rmd')"
+export USER=sug335
+export PASSWORD=northCountry2020
+Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
 Rscript neotomaplots.R
+git add --all
+git commit -m "Running the build"
+git push
