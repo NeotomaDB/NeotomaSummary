709048d278b26995a99cfb675699262e6698e669
Simon Goring <simon.j.goring@gmail.com>
2022-07-22 09:24:13 -0700

    Running the build

--- buildStats.sh
+++ buildStats.sh
@@ -5,7 +5,6 @@ export PORT=5432
 export USER=sug335
 export PASSWORD=northCountry2020
 Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
-Rscript neotomaplots.R
 git add --all
 git commit -m "Running the build"
 git push
