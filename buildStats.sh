96aab08f8cfcd140aca53876d17dd16574399e63
Simon Goring <simon.j.goring@gmail.com>
2022-07-28 10:39:16 -0700

    Running the build

--- buildStats.sh
+++ buildStats.sh
@@ -4,6 +4,7 @@ export HOST=db5.cei.psu.edu
 export PORT=5432
 export USER=sug335
 export PASSWORD=northCountry2020
+bash ~/Documents/Neotoma/logs/calllogs.sh
 Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
 git add --all
 git commit -m "Running the build"
