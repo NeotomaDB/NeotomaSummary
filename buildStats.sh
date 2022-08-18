2facf64d61d5c30d4cff364546d2b0a30640b9b3
Simon Goring <simon.j.goring@gmail.com>
2022-08-18 15:58:50 -0700

    Running the build

--- buildStats.sh
+++ buildStats.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-export DBNAME=neotoma
-export HOST=db5.cei.psu.edu
-export PORT=5432
-export USER=sug335
-export PASSWORD=northCountry2020
-Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
-git add --all
-git commit -m "Running the build"
-git push
+
+if [$1 = "local"]; then
+    echo local
+    export DBNAME=neotoma
+    export HOST=localhost
+    export PORT=5432
+    export USER=postgres
+    export PASSWORD=postgres
+else
+    export DBNAME=neotoma
+    export HOST=db5.cei.psu.edu
+    export PORT=5432
+    export USER=sug335
+    export PASSWORD=northCountry2020
+    # Rscript -e "rmarkdown::render('StateoftheDB.Rmd')"
+    git add --all
+    git commit -m "Running the build"
+    git push
+fi
+echo Done.
