92c2d728bafe0d79c7a93f572b2c40ed4227270b
Simon <simon.j.goring@gmail.com>
2024-09-04 17:07:22 -0700

    Running the build

--- run_cloudwatchquery.sh
+++ run_cloudwatchquery.sh
@@ -21,5 +21,28 @@ queryId=$(aws logs start-query \
 echo "Query started (query id: $    --query-id $queryId), please hold ..." && sleep 300 # give it some time to query
 aws logs get-query-results \
     --query-id $queryId \
-    --output json > log_run_calls.json
+    --output json > log_run_api_calls.json
 
+## Tilia Daily Calls:
+queryId=$(aws logs start-query \
+    --log-group-name '/aws/elasticbeanstalk/Tiliaapiv2-env/var/log/nginx/access.log' \
+    --start-time `date -d '-1 year' +"%s"` \
+    --end-time `date +"%s"` \
+    --query-string 'fields @message | parse @message "* - - [*] \"* *?* *\" * * *" as loggingIP, loggingTime, loggingVerb, loggingPath, loggingQuery, loggingUse, loggingStatus, loggingSize | filter (loggingVerb = "GET" or loggingVerb = "POST") and loggingStatus = 200  | stats count(*) as calls by datefloor(@timestamp, 1d) as date | sort by date desc' | jq -r '.queryId')
+
+echo "Query started (query id: $    --query-id $queryId), please hold ..." && sleep 300 # give it some time to query
+aws logs get-query-results \
+    --query-id $queryId \
+    --output json > log_run_tilia.json
+
+## Tilia Steward Calls
+queryId=$(aws logs start-query \
+    --log-group-name '/aws/elasticbeanstalk/Tiliaapiv2-env/var/log/nginx/access.log' \
+    --start-time `date -d '-1 year' +"%s"` \
+    --end-time `date +"%s"` \
+    --query-string 'fields @message | filter strcontains(@message, "validate") | parse @message "* - - [*] \"* *?*_username=%27*%27 *\" * * *" as loggingIP, loggingTime, loggingVerb, loggingPath, loggingQuery, loggingSteward, loggingUse, loggingStatus, loggingSize | filter (loggingVerb = "GET" or loggingVerb = "POST") and loggingStatus = 200 | stats count(*) as count by loggingSteward | sort by @timestamp desc' | jq -r '.queryId')
+
+echo "Query started (query id: $    --query-id $queryId), please hold ..." && sleep 300 # give it some time to query
+aws logs get-query-results \
+    --query-id $queryId \
+    --output json > log_run_tilia_steward.json
